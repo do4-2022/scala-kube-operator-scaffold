7581c34a8fc4e335a28fd8fb10fdb16fa9353093
iverly <github@iverly.net>
2023-06-14 14:44:56 +0200

    feat: add nextra quickstart
    Signed-off-by: iverly <github@iverly.net>

---
+++ docs/theme.config.jsx
@@ -0,0 +1,8 @@
+export default {
+  logo: <strong>Kubernetes Operator Scaffold</strong>,
+  docsRepositoryBase:
+    'https://github.com/do4-2022/scala-kube-operator-scaffold/blob/main/docs',
+  project: {
+    link: 'https://github.com/do4-2022/scala-kube-operator-scaffold',
+  },
+}
